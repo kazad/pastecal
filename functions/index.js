5dd46ee4b1d2ed028f04e82987ed68909629299e
Kalid Azad <kalid.azad@gmail.com>
2023-08-02 12:00:47 -0700

    add ICS integration

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,59 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+function jsonToIcs(json, id) {
+    let icsEvents = [];
+
+    json.events.forEach(event => {
+        let icsEvent = [
+            "BEGIN:VEVENT",
+            `UID:${event.id}`,
+            `DTSTART:${event.start.replace(/[-:]/g, '').replace('.000Z', 'Z')}`,
+            `DTEND:${event.end.replace(/[-:]/g, '').replace('.000Z', 'Z')}`,
+            `SUMMARY:${event.title}`,
+            `DESCRIPTION:${event.description}`,
+        ];
+
+        if (event.recurrencerule) {
+            icsEvent.push(`RRULE:${event.recurrencerule}`);
+        }
+
+        icsEvent.push("END:VEVENT");
+
+        icsEvents.push(icsEvent.join("\n"));
+    });
+
+    let icsFile = [
+        "BEGIN:VCALENDAR",
+        "VERSION:2.0",
+        "PRODID:-//PasteCal//" + id + "//EN",
+        ...icsEvents,
+        "END:VCALENDAR"
+    ].join("\n");
+
+    return icsFile;
+}
+
+exports.generateICS = functions.https.onRequest((req, res) => {
+    let id = req.path.split('/')[1]; // Extract the ID from the URL path
+    id = id.replace(/[.]ICS.*/i, '');
+
+    console.log('Generating ICS for calendar ID', req.path, id);
+
+    // Fetch the calendar data for this ID from your database
+    admin.database().ref('/calendars').child(id).once('value', snapshot => {
+        const calendarData = snapshot.val();
+
+        if (!calendarData) {
+            res.status(404).send('Calendar not found');
+            return;
+        }
+
+        const icsData = jsonToIcs(calendarData, id); // Call your jsonToIcs function
+
+        res.set('Content-Type', 'text/calendar');
+        res.send(icsData);
+    });
+});
