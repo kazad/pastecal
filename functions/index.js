d33fa8c298da5df0c6abf32b1eae14d62019cd31
Kalid Azad <kalid.azad@gmail.com>
2024-09-29 13:12:15 -0700

    uuid

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { v4: uuidv4 } = require('uuid'); // Import uuid for generating GUIDs
 
 admin.initializeApp();
 
