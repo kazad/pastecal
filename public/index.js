746936065755334d0b530aec41fb38e36a82f406
Kalid Azad <kalid.azad@gmail.com>
2024-10-22 15:10:26 -0700

    Recent calendars feature. Toolbar cleanups.

--- public/index.js
+++ public/index.js
@@ -22,6 +22,63 @@ Utils.nanoid = nanoid;
 
 // can we make this a mutator, where the get/set of a property on this automatically syncs to localstorage?
 // TODO: find localstorage sync?
+class RecentCalendars {
+    constructor() {
+        this.load();
+    }
+
+    load() {
+        this.items = JSON.parse(localStorage.getItem('recentCalendars')) || [];
+    }
+
+    save() {
+        localStorage.setItem('recentCalendars', JSON.stringify(this.items));
+    }
+
+    add(id, title) {
+        // Remove if exists
+        this.items = this.items.filter(item => item.id !== id);
+        
+        // Add to front
+        this.items.unshift({
+            id: id,
+            title: title || id,
+            pinned: false,
+            lastVisited: new Date().toISOString()
+        });
+
+        // Keep only last 10 unpinned items
+        const pinnedItems = this.items.filter(item => item.pinned);
+        const unpinnedItems = this.items.filter(item => !item.pinned).slice(0, 10);
+        this.items = [...pinnedItems, ...unpinnedItems];
+
+        this.save();
+    }
+
+    remove(id) {
+        this.items = this.items.filter(item => item.id !== id);
+        this.save();
+    }
+
+    togglePin(id) {
+        const item = this.items.find(item => item.id === id);
+        if (item) {
+            item.pinned = !item.pinned;
+            this.save();
+        }
+    }
+
+    getAll() {
+        return [...this.items].sort((a, b) => {
+            // Pinned items first
+            if (a.pinned && !b.pinned) return -1;
+            if (!a.pinned && b.pinned) return 1;
+            // Then by last visited
+            return new Date(b.lastVisited) - new Date(a.lastVisited);
+        });
+    }
+}
+
 var _calstore = _calstore ?? {};
 
 Utils.init = function () {
