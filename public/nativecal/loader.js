e8cf8d02318212ae57528c478de849f8c393654e
Kalid Azad <kalid.azad@gmail.com>
2025-11-21 18:06:14 -0800

    much better nativecal

---
+++ public/nativecal/loader.js
@@ -0,0 +1,25 @@
+(function() {
+    const timestamp = new Date().getTime();
+    const files = [
+        '/nativecal/components/NativeCalendar.js',
+        '/nativecal/app.js'
+    ];
+
+    function loadScript(index) {
+        if (index >= files.length) return;
+        
+        const src = files[index];
+        const script = document.createElement('script');
+        script.src = src + '?v=' + timestamp;
+        script.async = false;
+        script.onload = function() {
+            loadScript(index + 1);
+        };
+        script.onerror = function() {
+            console.error('Failed to load script: ' + src);
+        };
+        document.body.appendChild(script);
+    }
+
+    loadScript(0);
+})();
